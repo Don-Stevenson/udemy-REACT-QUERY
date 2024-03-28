3290f7d7fe1cb2e4f2fe24f6030eae0603e2e06d
Don Stevenson <dstevenson@tribalscale.com>
2024-03-28 14:07:34 -0400

    Complete base blog, update, delete, prefect blogs

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,21 +1,60 @@
-import { fetchComments } from "./api";
-import "./PostDetail.css";
+import { fetchComments } from './api'
+import { useQuery } from '@tanstack/react-query'
+import './PostDetail.css'
 
-export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+export function PostDetail({ post, deleteMutation, updateMutation }) {
+  const { data, isLoading, error, isError } = useQuery({
+    queryKey: ['comments', post.id],
+    queryFn: () => fetchComments(post.id),
+    staleTime: 2000,
+  })
 
+  if (isLoading) {
+    return <div>....loading </div>
+  }
+  if (isError) {
+    return <h3>oops, an error has occured {error.toString()} </h3>
+  }
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <h3 style={{ color: 'blue' }}>{post.title}</h3>
+      <div>
+        <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+        {deleteMutation.isPending && (
+          <p className="loading"> Deleting the post</p>
+        )}
+        {deleteMutation.isError && (
+          <p className="error">
+            Error deleting the post: {deleteMutation.error.toString()}
+          </p>
+        )}
+        {deleteMutation.isSuccess && (
+          <p className="success"> The post was deleted</p>
+        )}
+      </div>
+      <div>
+        <button onClick={() => updateMutation.mutate(post.id)}>
+          Update title
+        </button>
+        {updateMutation.isPending && (
+          <p className="loading"> Updating the post</p>
+        )}
+        {updateMutation.isError && (
+          <p className="error">
+            Error updating the post: {updateMutation.error.toString()}
+          </p>
+        )}
+        {updateMutation.isSuccess && (
+          <p className="success"> The post was updated</p>
+        )}
+      </div>
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
+      {data.map(comment => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
         </li>
       ))}
     </>
-  );
+  )
 }
