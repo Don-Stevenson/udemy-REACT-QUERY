ba41db8838a77aa063c3c54c7847b5df8662e33f
Don Stevenson <dstevenson@tribalscale.com>
2024-04-01 16:40:14 -0400

    Add prefetching data for the next month

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -6,19 +6,26 @@ import { filterByTreatment } from "../utils";
 
 import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
+import { useQuery } from "@tanstack/react-query";
 
-// query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+// query function for useQuery 
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
 
-export function useStaff() {
+export function useStaff(): Staff[] {
+
+  const fallBack: Staff[] = []
   // for filtering staff by treatment
+
+  const {data: staff = fallBack} = useQuery({
+    queryKey: [queryKeys.staff],
+    queryFn: getStaff,
+  })
   const [filter, setFilter] = useState("all");
 
   // TODO: get data from server via useQuery
-  const staff: Staff[] = [];
-
+ 
   return { staff, filter, setFilter };
 }
